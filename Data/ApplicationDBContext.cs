f4e31113024442cc748431faf40c2d751c08a154
Momcilo Savic <savic227@gmail.com>
2024-08-12 09:41:36 +0200

    Add register user API endpoint

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,19 +19,35 @@ namespace api.Data
         public DbSet<Todo> Todos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
+        {
+            base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Todo>()
-            .HasOne(t => t.User)
-            .WithMany(u => u.Todos)
-            .HasForeignKey(t => t.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+            
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
 
-        modelBuilder.Entity<User>()
-            .HasMany(u => u.Todos)
-            .WithOne(t => t.User)
-            .HasForeignKey(t => t.UserId);
-    }
+            modelBuilder.Entity<Todo>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Todos)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Todos)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId);
+        }
     }
 }
---
+++ Migrations/20240812073550_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "579012cf-45b8-4ada-adca-c3bd7bf113dd", null, "Admin", "ADMIN" },
+                    { "e8a581e9-6987-4468-9b8a-ebf2f39725dc", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "579012cf-45b8-4ada-adca-c3bd7bf113dd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e8a581e9-6987-4468-9b8a-ebf2f39725dc");
+        }
+    }
+}
